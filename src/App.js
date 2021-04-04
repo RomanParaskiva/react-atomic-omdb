3888c21ebec561b1873bc5d0823a05e8f8b90dfa
RP <parasta2018@gmail.com>
2021-04-04 22:22:21 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
 
-function App() {
+import Header from './components/Header'
+import Movie from './components/Movie'
+import Search from './components/Search'
+
+import './App.css'
+
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=3aa396"
+
+
+const App = () => {
+
+  const [loading, setLoading] = useState(true),
+        [movies, setMovies] = useState([]),
+        [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(async () => {
+    const res = await fetch(MOVIE_API_URL),
+          json = await res.json()
+        setMovies(json.Search)
+        setLoading(false)
+  }, [])
+
+  const search = async (searchValue) => {
+    setLoading(true)
+    setErrorMessage(null)
+
+   const res = await fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=3aa396`),
+        json = await res.json()
+
+        if (json.Response === "True") {
+          setMovies(json.Search);
+          setLoading(false);
+        } else {
+          setErrorMessage(json.Error);
+          setLoading(false);
+        }
+   
+    setLoading(false)
+    console.log(searchValue)
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header text="HOOKED" />
+      <Search search={search} />
+      <p className="App-intro">Sharing a few of our favourite movies</p>
+      <div className="movies">
+        {loading && !errorMessage ? (
+         <span>loading...</span>
+         ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          movies.map((movie, index) => (
+            <Movie key={`${index}-${movie.Title}`} movie={movie} />
+          ))
+        )}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/Search.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const Search = (props) => {
+  const [searchValue, setSearchValue] = useState('')
+
+  const handleSearchValue = (e) => {
+    setSearchValue(e.target.value)
+  }
+
+  const clearSearchValue = () => {
+    setSearchValue('')
+  }
+
+  const runSearch = (e) => {
+    e.preventDefault()
+    props.search(searchValue)
+    clearSearchValue()
+  }
+
+  return (
+    <form className="search">
+      <input 
+        value={searchValue}
+        onChange={handleSearchValue}
+        type="text" 
+      />
+
+      <input onClick={runSearch} type="submit" value="SEARCH" />
+    </form>
+  )
+}
+
+export default Search
