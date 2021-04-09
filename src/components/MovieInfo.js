f4f56239d5956be8fbfb2092d1c6f8601ac605b0
RomanParaskiva <parasta2018@gmail.com>
2021-04-09 15:22:28 +0300

    dsd

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -1,18 +1,60 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
-const MovieInfo = ({ movie }) => {
-    console.log(movie)
-    const IMG_API_URL = "https://image.tmdb.org/t/p/w500"
+const MovieInfo = ({ movie, openHandler }) => {
+    const IMG_API_URL = "https://image.tmdb.org/t/p/w500",
+        [actors, setActors] = useState([])
+    let actorsList, filmsList = []
+
+    const handler = () => {
+        openHandler(null)
+    }
+
+    const getCredits = async (id) => {
+        const res = await axios(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=15f70723a36f993b310bad745e6681ed&language=ru-RU`)
+        res.status === 200 && setActors(res.data.cast)
+    }
+
+    const getPersonMovies = async (e) => {
+        const res = await axios(`https://api.themoviedb.org/3/person/${e.target.dataset.id}/movie_credits?api_key=15f70723a36f993b310bad745e6681ed&language=ru-RU`)
+        console.log(res)
+        filmsList = res.data.cast.map(item =>
+            <img alt={item.character} src={IMG_API_URL + item.backdrop_path} />
+        )
+        console.log(filmsList)
+    }
+
+    useEffect(() => {
+        getCredits(movie.id)
+    }, [movie.id])
+
+    actorsList = actors.map(item => {
+        return <li onClick={getPersonMovies} data-id={item.id} key={item.id}>
+            {item.original_name}
+            <img loading="lazy" alt={item.name} src={IMG_API_URL + item.profile_path} />
+        </li>
+    })
+
+
+
     return (
-        <div className="modal">
-            <i className="material-icons">clear</i>
+        <div className="modal card-panel">
+            <i className="material-icons modal-close" onClick={handler}>clear</i>
+            <ul>
+                { filmsList ? filmsList : '' }
+            </ul>
             <div className="left">
                 <img src={IMG_API_URL + movie.backdrop_path} alt={movie.title} />
+                <h5>{movie.title}</h5>
+                <span>{movie.original_title}</span>
+                <p>{movie.overview}</p>
+                <span>Популярность : {movie.popularity} / Голоса : {movie.vote_count}</span>
             </div>
 
             <div className="right">
-                <h5>{movie.title}</h5>
-                <p>{movie.overview}</p>
+                <ul>
+                    {actorsList}
+                </ul>
             </div>
         </div>
     )
