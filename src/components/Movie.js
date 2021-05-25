7f634cc9bf1dd917b96857012cbdd66ffd736021
Roman Paraskiva <parasta2018@gmail.com>
2021-05-25 22:10:52 +0400

    repair movie image

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const DEFAULT_PLACEHOLDER_IMAGE = "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg",
+const DEFAULT_PLACEHOLDER_IMAGE = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png",
   IMG_API_URL = "https://image.tmdb.org/t/p/w500"
 
-const Movie = ({ movie, openHandler }) => {
-  const poster = !movie.poster_path ? DEFAULT_PLACEHOLDER_IMAGE : movie.poster_path
-    let overview
+const Movie = ({
+  movie,
+  openHandler
+}) => {
+  const poster = !movie.poster_path ? DEFAULT_PLACEHOLDER_IMAGE : IMG_API_URL + movie.poster_path
+  let overview
 
   overview = movie.overview ? movie.overview : 'Описание не найдено'
 
@@ -13,22 +16,20 @@ const Movie = ({ movie, openHandler }) => {
     openHandler(movie.id)
   }
   return (
-    <a onClick={handler} href="#!">
+    <a onClick = {handler} href = "#!">
       <div className="movie">
-        <h5>{movie.title}</h5>
-        <div className="movie__img-wrapper">
-          <img
-            width="200"
-            alt={`Название: ${movie.title}`}
-            src={IMG_API_URL + poster}
-          />
-          <div className="movie__reviews">
-          {overview}
-          </div>
-        </div>
+
+        <h5>{movie.title}</h5> 
+        <div className="movie__img-wrapper"> 
+          <img width = "200"
+            alt = {`Название: ${movie.title}`}
+            src = {poster}
+          /> 
+          <div className="movie__reviews"> {overview} </div> 
+        </div> 
         <p>({movie.release_date})</p>
 
-      </div>
+      </div> 
     </a>
   )
 }
